16f28aff7198168e4e811d557b857b3bc14be278
Hung DO <hungdohuy@gmail.com>
2025-06-13 16:15:51 +0700

    update workflow default

--- provisioning.sh
+++ provisioning.sh
@@ -21,7 +21,7 @@ NODES=(
 )
 
 WORKFLOWS=(
-    "https://gist.githubusercontent.com/robballantyne/f8cb692bdcd89c96c0bd1ec0c969d905/raw/2d969f732d7873f0e1ee23b2625b50f201c722a5/flux_dev_example.json"
+    "https://raw.githubusercontent.com/Rio277/provisioning-scripts/refs/heads/main/default-flux-wf.json"
 )
 
 CLIP_MODELS=(
